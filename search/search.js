20f353cae23f73e30943b17cfc925fe443f1ac84
Numenta CI <numenta-ci@numenta.com>
2017-02-03 01:01:50 +0000

    Development documentation build.

--- search/search.js
+++ search/search.js
@@ -7,30 +7,10 @@
 
 var indexSectionsWithContent =
 {
-  0: "_abcdefghilmnoprstuvw~",
-  1: "cdilnorst",
-  2: "n",
-  3: "cdilnorst",
-  4: "_abcdefghilmnoprstuvw~",
-  5: "bchinrsu",
-  6: "ln",
-  7: "ln",
-  8: "no",
-  9: "t"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "namespaces",
-  3: "files",
-  4: "functions",
-  5: "typedefs",
-  6: "enums",
-  7: "enumvalues",
-  8: "related",
-  9: "pages"
 };
 
 function convertToId(search)
